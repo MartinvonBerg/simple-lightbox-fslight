e28cee777396953f04d3e4b6934df6290a1d20f5
Martin <mail@mvb1.de>
2022-04-19 08:55:35 +0200

    class generated

--- lightbox-gallery-by-kodefix.php
+++ lightbox-gallery-by-kodefix.php
@@ -25,113 +25,142 @@ if ( ! defined('ABSPATH' )) { die('Are you ok?');}
 require_once __DIR__ . "/vendor/autoload.php";
 const ALLOW_DUPLICATE_IDS = 67108864;
 
-// --------------- settings ----------------------------------------
-$page = true;
-$post = true;
-$home = true;
-$front = true;
-
-$archive = false;
-$date = false;
-$author = false;
-$tag = false;
-$category = false;
-$attachment = false;
-
-$hrefEmpty = true;
-$hrefattach = false;
-$hrefMedia = true;
-
-// --------------- settings end ------------------------------------
-
-function lightbox_gallery_for_gutenberg($content)
+final class RewriteFigureTags
 {
-    
-    $dom = new \IvoPetkov\HTML5DOMDocument();
-    $dom->loadHTML($content, ALLOW_DUPLICATE_IDS);
+    // --------------- settings ----------------------------------------
+    // PHP 7.3 version :: no type definition
+    protected $page = true;
+    protected $post = true;
+    protected $home = true;
+    protected $front = true;
 
-    $allFigures = $dom->querySelectorAll('figure');
-    $nFound = 0;
-           
-    foreach ($allFigures as $figure) {
+    protected $archive = false;
+    protected $date = false;
+    protected $author = false;
+    protected $tag = false;
+    protected $category = false;
+    protected $attachment = false;
+    protected $siteUrl  = '';
 
-        $class = $figure->getAttribute("class");
-        $found = 0;
-        $isMediaFile = 0;
-        $found =strpos($class, 'image');
-        $item = $figure->querySelector("img");
+    protected $hrefEmpty = true;
+    protected $hrefMedia = true;
+    protected $hrefAttach = false; // only for information. No Functionality provided.
+    protected $hrefExternal = false; // only for information. No Functionality provided.
 
-        //if (\current_user_can('edit_posts')) echo "fslight" . $class;
-        
-        if (is_null($item) ) {
-            $found = 0;
-        }
+    protected $cssClassesToSearch = [
+        'block-image',
+        'media-text',
+        'block-video'
 
-        $caption = $figure->querySelector("figcaption");
+    ];
 
-        $href = '';
-        $hasHref = true;
-        $hasHref = $figure->querySelector("a");
-        
-        if (! is_null($hasHref)) {
-            $href = $hasHref->getAttribute('href');
-            $isMediaFile = \strpos( $href, 'uploads');
+    public function __construct()
+    {
+       $this->siteUrl = \get_site_url();
+       // todo: load settings from json
+    }
 
-            if (!filter_var($href, FILTER_VALIDATE_URL) === false) {
-                //break;
-              } 
-        }
-        
-        if (($found > 0) && ( is_null($hasHref) || ($isMediaFile > 0) )) {
-            $newfigure = $dom->createElement("figure");
-            $newfigure->setAttribute("class", $class);
-            $a = $dom->createElement("a");
-            $a->setAttribute("data-fslightbox", true);
-            $a->setAttribute("data-type", "image");
-            $a->setAttribute("href", $item->getAttribute("src"));
-            $a->appendChild($item);
-            $newfigure->appendChild($a);
-            if (! is_null($caption)) {
-                $newfigure->appendChild($caption);
+    private function findCssClass($class)
+    {
+        $classFound = false;
+        $isVideo = false;
+
+        foreach($this->cssClassesToSearch as $search) {
+            $classFound = 0;
+            $classFound =strpos($class, $search); // todo: include videos
+            if ($classFound > 0) {
+                $classFound = true;
+                break;
             }
-            $figure->parentNode->replaceChild($newfigure, $figure);
-            $nFound += 1;
+        }
+        $isVideo = ((strpos($search, 'video') > 0) && $classFound) ? true : false;
+        return [$classFound, $isVideo];
+    }
+
+    public function lightbox_gallery_for_gutenberg($content)
+    {
+        $dom = new \IvoPetkov\HTML5DOMDocument();
+        $dom->loadHTML($content, ALLOW_DUPLICATE_IDS);
+
+        $allFigures = $dom->querySelectorAll('figure');
+        $nFound = 0;
+            
+        foreach ($allFigures as $figure) {
+
+            $class = $figure->getAttribute("class");
+            [$classFound, $isVideo] = $this->findCssClass($class);
+
+            if ($classFound && ! $isVideo) {;
+
+                $item = $figure->querySelector("img");
+                if (is_null($item)) $classFound = false;
+
+                $href = '';
+                $href = $figure->querySelector("a");
+                $hasHref = \is_null($href) ? false : true;
+                $isMediaFile = false;
+                
+                if ( $hasHref ) {
+                    $href = $href->getAttribute('href');
+                    $isMediaFile = \strpos( $href, 'uploads');
+                    $hasSiteUrl = \strpos( $href, $this->siteUrl);
+                    if ($isMediaFile > 0 && $hasSiteUrl > 0)
+                        $isMediaFile = true;
+                }
+                    
+                if (($classFound > 0) && ( (!$hasHref && $this->hrefEmpty) || ($isMediaFile && $this->hrefMedia) )) {
+                    $newfigure = $dom->createElement("figure");
+                    $newfigure->setAttribute("class", $class);
+                    $a = $dom->createElement("a");
+                    $a->setAttribute("data-fslightbox", true);
+                    $a->setAttribute("data-type", "image");
+                    $a->setAttribute("href", $item->getAttribute("src"));
+                    $a->appendChild($item);
+                    $newfigure->appendChild($a);
+
+                    $caption = $figure->querySelector("figcaption");
+                    if (! is_null($caption)) {
+                        $newfigure->appendChild($caption);
+                    }
+                    $figure->parentNode->replaceChild($newfigure, $figure);
+                    $nFound += 1;
+                }
+            } elseif ($classFound && $isVideo) {
+                // <figure class="wp-block-video"><video controls preload="auto" src="http://localhost/wordpress/wp-content/uploads/2022/04/sample-mp4-file.mp4"></video></figure>
+                $item = $figure->querySelector("video"); // diff
+                $newfigure = $dom->createElement("figure");
+                $newfigure->setAttribute("class", $class);
+                $a = $dom->createElement("a");
+                $a->setAttribute("data-fslightbox", true);
+                $a->setAttribute("data-type", "video"); // diff
+                $a->setAttribute("href", $item->getAttribute("src"));
+                $a->appendChild($item);
+                $newfigure->appendChild($a);
+                $figure->parentNode->replaceChild($newfigure, $figure);
+                $nFound += 1;
+            }
         }
         
-    }
-    
-    if ( $nFound > 0) {
-        wp_enqueue_script( 
-            "engine", 
-            plugin_dir_url(__FILE__) . "/js/fslightbox.js", 
-            [], 
-            "3.3.1", 
-            true
-        );
-    }
-    
-    return $dom->saveHTML();
-    
+        if ( $nFound > 0) {
+            wp_enqueue_script( 
+                "engine", 
+                plugin_dir_url(__FILE__) . "/js/fslightbox.js", 
+                [], 
+                "3.3.1", 
+                true
+            );
+        }
+
+        return $dom->saveHTML();  
+    }  
 }
 
-add_filter("the_content", '\mvbplugins\fslightbox\lightbox_gallery_for_gutenberg', 10, 1);
 
+add_filter("the_content", '\mvbplugins\fslightbox\wrapClass', 10, 1);
 
-/**
- * Get the upload URL/path in right way (works with SSL).
- *
- * @param string $param  "basedir" or "baseurl"
- * @param string $subfolder  subfolder to append to basedir or baseurl
- * @return string the base appended with subfolder
- */
-function my_get_upload_dir($param, $subfolder = '') :string
+function wrapClass($content)
 {
-	$upload_dir = wp_get_upload_dir();
-	$url = $upload_dir[$param];
-
-	if ($param === 'baseurl' && is_ssl()) {
-		$url = str_replace('http://', 'https://', $url);
-	}
-
-	return $url . $subfolder;
+    $rewrite = new RewriteFigureTags();
+    $new=$rewrite->lightbox_gallery_for_gutenberg($content);
+    return $new;
 }
