2a69df533f65276b3e47cbccc0e290abd99689d1
Martin <mail@mvb1.de>
2022-04-19 14:05:10 +0200

    code rework

---
+++ classes/RewriteFigureTagsClass.php
@@ -0,0 +1,179 @@
+<?php
+/**
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Simple Lightbox for Gutenberg - Responsive Lightbox Effect for Image, Gallery and Media-Text Block.
+ * Plugin URI:        https://github.com/MartinvonBerg/simple-lightbox-gutenberg
+ * Description:       An easy way to create lightbox effect for WordPress Gutenberg images and galleries. Settings provided with json-File in Plugin-Folder.
+ * Version:           0.0.1
+ * Requires at least: 5.9
+ * Requires PHP       7.3
+ * Author:            Martin von Berg
+ * Author URI:        https://www.berg-reise-foto.de/software-wordpress-lightroom-plugins/wordpress-plugins-fotos-und-gpx/
+ * License:           GPL-2.0
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ *  
+ */
+
+namespace mvbplugins\fslightbox;
+
+// fallback for wordpress security
+if ( ! defined('ABSPATH' )) die('Are you ok?');
+
+require_once __DIR__ . "/vendor/autoload.php";
+const ALLOW_DUPLICATE_IDS = 67108864;
+
+// todo: load js from subfolder "paid" and fallback to standard if not available. Mind gitignore!
+final class RewriteFigureTags
+{
+    // --------------- settings ----------------------------------------
+    // PHP 7.3 version :: no type definition
+    protected $posttype = '';
+    protected $siteUrl  = '';
+    protected $doRewrite = false;
+    protected $hrefEmpty = false;
+    protected $hrefMedia = false;
+
+    protected $hrefTypes = [
+        'Empty',
+        'Media',
+    ];
+
+    protected $postTypes = [
+        'page',
+        'post',
+        //'attachment',
+        'home',
+        'front',
+        //'archive',
+        //'date',
+        //'author',
+        //'tag',
+        //'category',
+    ];
+
+    protected $cssClassesToSearch = [
+        'block-image',
+        'media-text',
+        'block-video',
+        'postie-image'
+    ];
+
+    public function __construct()
+    {
+       $this->siteUrl = \get_site_url();
+       $this->posttype = \get_post_type();
+       $this->doRewrite = in_array($this->posttype, $this->postTypes, true);
+
+       foreach($this->hrefTypes as $type) {
+           switch ( strtolower($type)) {
+                case 'empty':
+                    $this->hrefEmpty = true;
+                    break;
+                case 'media':
+                    $this->hrefMedia = true;
+                    break;
+                default:
+                    break;
+           }
+       }
+       // todo: load settings from json
+    }
+
+    private function findCssClass($class)
+    {
+        $classFound = false;
+        $isVideo = false;
+
+        foreach($this->cssClassesToSearch as $search) {
+            $classFound = 0;
+            $classFound =strpos($class, $search); 
+            if ($classFound !== false) {
+                $classFound = true;
+                break;
+            }
+        }
+        $isVideo = ((strpos($search, 'video') !== false) && $classFound) ? true : false;
+        return [$classFound, $isVideo];
+    }
+
+    private function my_enqueue_script() {
+        wp_enqueue_script( "fslightbox", plugin_dir_url(__FILE__) . "/js/fslightbox-basic/fslightbox.js", [], "3.3.1", true );
+    }
+
+    public function lightbox_gallery_for_gutenberg($content)
+    {
+        if ( ! $this->doRewrite) return $content;
+
+        $dom = new \IvoPetkov\HTML5DOMDocument();
+        $dom->loadHTML($content, ALLOW_DUPLICATE_IDS);
+
+        $allFigures = $dom->querySelectorAll('figure');
+
+        // append all old divs to $allFigures. These are converted to figures.
+        $allImgs = $dom->querySelectorAll('img');
+        foreach($allImgs as $image) {
+            $parent = $image->parentNode->parentNode; // todo: what if there is no a tag? Ignore, because wont't work anyway?
+            $tag = $parent->tagName;
+            if ($tag === 'div') {
+                $allFigures->append($parent);
+            }
+        }
+        $nFound = 0;
+            
+        foreach ($allFigures as $figure) {
+
+            $class = $figure->getAttribute("class");
+            $classFound = false;
+            [$classFound, $isVideo] = $this->findCssClass($class);
+            $isMediaFile = false;
+            $hasHref = false;
+            $item = null;
+
+            if ($classFound && ! $isVideo) 
+            {
+                $item = $figure->querySelector( 'img');
+                $dataType = 'image';  
+
+                $href = null;
+                $href = $figure->querySelector("a");
+                $hasHref = \is_null($href) ? false : true;
+                            
+                if ( $hasHref ) {
+                    $href = $href->getAttribute('href');
+                    $isMediaFile = \strpos( $href, 'uploads');
+                    $hasSiteUrl = \strpos( $href, $this->siteUrl);
+                    if ( ($isMediaFile !== false) && ($hasSiteUrl !== false) )
+                        $isMediaFile = true;
+                }  
+            }
+            elseif ($classFound && $isVideo) 
+            {   
+                $item = $figure->querySelector( 'video');
+                $dataType = 'video';
+            } 
+           
+            if ( ($classFound) && (! is_null($item)) && ( ( ! $hasHref && $this->hrefEmpty) || ($isMediaFile && $this->hrefMedia) || $isVideo )) {
+                $newfigure = $dom->createElement("figure");
+                $newfigure->setAttribute("class", $class);
+                $a = $dom->createElement("a");
+                $a->setAttribute("data-fslightbox", true);
+                $a->setAttribute("data-type", $dataType);
+                $a->setAttribute("href", $item->getAttribute("src"));
+                $a->appendChild($item);
+                $newfigure->appendChild($a);
+
+                $caption = $figure->querySelector("figcaption");
+                if (! is_null($caption)) {
+                    $newfigure->appendChild($caption);
+                }
+                $figure->parentNode->replaceChild($newfigure, $figure);
+                $nFound += 1;   
+            } 
+        }
+        
+        if ( $nFound > 0) $this->my_enqueue_script();
+
+        return $dom->saveHTML();  
+    }  
+}
---
+++ fslightbox-gallery.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Simple Lightbox for Gutenberg
+ * Plugin URI:        https://github.com/MartinvonBerg/simple-lightbox-gutenberg
+ * Description:       An easy way to create lightbox effect for WordPress Gutenberg images, galleries and Media-Text-Block. Settings provided with json-File in Plugin-Folder, see there and Readme.
+ * Version:           1.2.0
+ * Requires at least: 5.9
+ * Requires PHP       7.3
+ * Author:            Martin von Berg
+ * Author URI:        https://www.berg-reise-foto.de/software-wordpress-lightroom-plugins/wordpress-plugins-fotos-und-gpx/
+ * License:           GPL-2.0
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ *  
+ */
+
+namespace mvbplugins\fslightbox;
+
+// fallback for wordpress security
+if ( ! defined('ABSPATH' )) die('Are you ok?');
+
+require_once __DIR__ . '/classes/RewriteFigureTagsClass.php';
+
+add_filter("the_content", '\mvbplugins\fslightbox\wrapClass', 10, 1);
+
+function wrapClass($content)
+{
+    $rewrite = new RewriteFigureTags();
+    $new=$rewrite->lightbox_gallery_for_gutenberg($content);
+    $rewrite = null;
+    return $new;
+}
