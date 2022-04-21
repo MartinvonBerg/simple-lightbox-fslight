a66752635d81f0b4aef2601bbfd638cad6277016
Martin <mail@mvb1.de>
2022-04-21 15:26:54 +0200

    coding guidelines

--- classes/RewriteFigureTagsClass.php
+++ classes/RewriteFigureTagsClass.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *
  * @wordpress-plugin
@@ -18,7 +19,7 @@
 namespace mvbplugins\fslightbox;
 
 // fallback for wordpress security
-if ( ! defined('ABSPATH' )) die('Are you ok?');
+if (!defined('ABSPATH')) die('Are you ok?');
 
 require_once \WP_PLUGIN_DIR . "/simple-lightbox-gutenberg/vendor/autoload.php"; // @phpstan-ignore-line
 const ALLOW_DUPLICATE_IDS = 67108864;
@@ -34,7 +35,7 @@ final class RewriteFigureTags
 {
     // --------------- settings ----------------------------------------
     // PHP 7.3 version :: no type definition
-    
+
     protected $posttype = '';
     protected $siteUrl  = '';
     protected $doRewrite = false;
@@ -75,48 +76,48 @@ final class RewriteFigureTags
     protected bool $hrefMedia = false;
     */
     /**
-	 * @var hrefTypes
-	 */
+     * @var hrefTypes
+     */
     # protected array $hrefTypes = [ 'Empty', 'Media' ];
 
     /**
-	 * @var postTypes
-	 */
+     * @var postTypes
+     */
     #protected array $postTypes = [ 'page', 'post',  'home', 'front', ];
-        //'attachment',
-        //'archive',
-        //'date',
-        //'author',
-        //'tag',
-        //'category'
+    //'attachment',
+    //'archive',
+    //'date',
+    //'author',
+    //'tag',
+    //'category'
 
     /**
-	 * @var cssClassesToSearch
-	 */
+     * @var cssClassesToSearch
+     */
     #protected array $cssClassesToSearch = [ 'block-image','media-text', 'block-video', 'postie-image' ];
-    
-    
+
+
     /**
      * Do settings for the class. Load from json-settings-file.
      */
     public function __construct()
     {
-       $this->siteUrl = \get_site_url();
-       $this->posttype = strval( \get_post_type() );
-       $this->doRewrite = in_array($this->posttype, $this->postTypes, true);
+        $this->siteUrl = \get_site_url();
+        $this->posttype = strval(\get_post_type());
+        $this->doRewrite = in_array($this->posttype, $this->postTypes, true);
 
-       // load settings from file plugin-settings.json
-       $path = \WP_PLUGIN_DIR . "/simple-lightbox-gutenberg/plugin-settings.json"; // @phpstan-ignore-line
-       if (is_file($path)) {
-           $settings = strval( file_get_contents( $path, false ) );
-           $settings = \json_decode( $settings, true );
-           $this->hrefTypes = $settings['hrefTypes'];
-           $this->postTypes = $settings['postTypes'];
-           $this->cssClassesToSearch = $settings['cssClassesToSearch'];
-       };
+        // load settings from file plugin-settings.json
+        $path = \WP_PLUGIN_DIR . "/simple-lightbox-gutenberg/plugin-settings.json"; // @phpstan-ignore-line
+        if (is_file($path)) {
+            $settings = strval(file_get_contents($path, false));
+            $settings = \json_decode($settings, true);
+            $this->hrefTypes = $settings['hrefTypes'];
+            $this->postTypes = $settings['postTypes'];
+            $this->cssClassesToSearch = $settings['cssClassesToSearch'];
+        };
 
-       foreach($this->hrefTypes as $type) {
-           switch ( strtolower($type) ) {
+        foreach ($this->hrefTypes as $type) {
+            switch (strtolower($type)) {
                 case 'empty':
                     $this->hrefEmpty = true;
                     break;
@@ -125,8 +126,8 @@ final class RewriteFigureTags
                     break;
                 default:
                     break;
-           }
-       }
+            }
+        }
     }
 
     /**
@@ -135,15 +136,15 @@ final class RewriteFigureTags
      * @param  string $class the class-attribute as string  
      * @return array{bool, bool}
      */
-    private function findCssClass( string $class)
+    private function findCssClass(string $class)
     {
         $classFound = false;
         $isVideo = false;
         $search = '';
 
-        foreach($this->cssClassesToSearch as $search) {
+        foreach ($this->cssClassesToSearch as $search) {
             $classFound = 0;
-            $classFound =strpos($class, $search); 
+            $classFound = strpos($class, $search);
             if ($classFound !== false) {
                 $classFound = true;
                 break;
@@ -158,18 +159,18 @@ final class RewriteFigureTags
      *
      * @return void
      */
-    private function my_enqueue_script() 
+    private function my_enqueue_script()
     {
         $path = \WP_PLUGIN_DIR . '/simple-lightbox-gutenberg/js/fslightbox-paid/fslightbox.js'; // @phpstan-ignore-line
         if (is_file($path)) {
             $path = \WP_PLUGIN_URL . '/simple-lightbox-gutenberg/js/fslightbox-paid/fslightbox.js'; // @phpstan-ignore-line
-            wp_enqueue_script( "fslightbox", $path, [], "3.4.1", true );
+            wp_enqueue_script("fslightbox", $path, [], "3.4.1", true);
         }
 
         $path = \WP_PLUGIN_DIR . '/simple-lightbox-gutenberg/js/fslightbox-basic/fslightbox.js'; // @phpstan-ignore-line
         if (is_file($path)) {
             $path = \WP_PLUGIN_URL . '/simple-lightbox-gutenberg/js/fslightbox-basic/fslightbox.js'; // @phpstan-ignore-line
-            wp_enqueue_script( "fslightbox", $path, [], "3.3.1", true );
+            wp_enqueue_script("fslightbox", $path, [], "3.3.1", true);
         }
     }
 
@@ -181,7 +182,7 @@ final class RewriteFigureTags
      */
     public function lightbox_gallery_for_gutenberg($content)
     {
-        if ( ! $this->doRewrite ) return $content;
+        if (!$this->doRewrite) return $content;
 
         $dom = new \IvoPetkov\HTML5DOMDocument();
         $dom->loadHTML($content, ALLOW_DUPLICATE_IDS);
@@ -190,7 +191,7 @@ final class RewriteFigureTags
 
         // append all old imgs in <div><a><img>..</img></a></div> to $allFigures. These are converted to figures.
         $allImgs = $dom->querySelectorAll('img');
-        foreach($allImgs as $image) {
+        foreach ($allImgs as $image) {
             $parent = $image->parentNode->parentNode; // todo: what if there is no a tag? Ignore, because wont't work anyway?
             $tag = $parent->tagName;
             if ($tag === 'div') {
@@ -198,7 +199,7 @@ final class RewriteFigureTags
             }
         }
         $nFound = 0;
-            
+
         foreach ($allFigures as $figure) {
 
             $class = $figure->getAttribute("class");
@@ -211,63 +212,60 @@ final class RewriteFigureTags
             $dataType = '';
 
             // provide item, $dataType, $isMediaFile, $hasHref from $figure, $classFound, $isVideo
-            if ($classFound && ! $isVideo) 
-            {
-                $item = $figure->querySelector( 'img');
-                $dataType = 'image';  
+            if ($classFound && !$isVideo) {
+                $item = $figure->querySelector('img');
+                $dataType = 'image';
 
                 $href = null;
                 $href = $figure->querySelector("a");
                 $hasHref = \is_null($href) ? false : true;
-                            
-                if ( $hasHref ) {
+
+                if ($hasHref) {
                     $href = $href->getAttribute('href');
-                    $header = \wp_remote_head($href, array( 'timeout'=>2 ));
-                    $content_type = wp_remote_retrieve_header( $header, 'content-type' );
-                    $isMediaFile = \strpos( $content_type, 'image');
-                    $hasSiteUrl = \strpos( $href, $this->siteUrl); // only shows lokal files in lightbox
+                    $header = \wp_remote_head($href, array('timeout' => 2));
+                    $content_type = wp_remote_retrieve_header($header, 'content-type');
+                    $isMediaFile = \strpos($content_type, 'image');
+                    $hasSiteUrl = \strpos($href, $this->siteUrl); // only shows lokal files in lightbox
                     $hasSiteUrl = true; // all files are shown, even externals. todo: remove this logic? Or keep for further extension?
-                    if ( ($isMediaFile !== false) && ($hasSiteUrl !== false) )
+                    if (($isMediaFile !== false) && ($hasSiteUrl !== false))
                         $isMediaFile = true;
-                }  
-            }
-            elseif ($isVideo) 
-            {   
-                $item = $figure->querySelector( 'video');
+                }
+            } elseif ($isVideo) {
+                $item = $figure->querySelector('video');
                 $dataType = 'video';
-            } 
-           
+            }
+
             // create new dom-element and append to dom
-            if ( ($classFound) && (! is_null($item)) && ( ( ! $hasHref && $this->hrefEmpty) || ($isMediaFile && $this->hrefMedia) || $isVideo )) {
+            if (($classFound) && (!is_null($item)) && ((!$hasHref && $this->hrefEmpty) || ($isMediaFile && $this->hrefMedia) || $isVideo)) {
                 $caption = $figure->querySelector("figcaption");
 
-                $newfigure = $dom->createElement( $tagType );
+                $newfigure = $dom->createElement($tagType);
                 $newfigure->setAttribute("class", $class);
 
                 $a = $dom->createElement("a");
                 $a->setAttribute("data-fslightbox", '1');
                 $a->setAttribute("data-type", $dataType);
-                if (! is_null($caption)) {
+                if (!is_null($caption)) {
                     $text = $caption->getNodeValue();
-                    $a->setAttribute("data-caption", $text );
+                    $a->setAttribute("data-caption", $text);
                 }
                 $a->setAttribute("href", $item->getAttribute("src"));
                 $a->appendChild($item);
 
                 $newfigure->appendChild($a);
-                
-                if (! is_null($caption)) {
+
+                if (!is_null($caption)) {
                     $newfigure->appendChild($caption);
                 }
 
                 $figure->parentNode->replaceChild($newfigure, $figure);
-                $nFound += 1;   
-            } 
+                $nFound += 1;
+            }
         }
-        
-        if ( $nFound > 0) 
+
+        if ($nFound > 0)
             $this->my_enqueue_script();
 
-        return $dom->saveHTML();  
-    }  
+        return $dom->saveHTML();
+    }
 }
